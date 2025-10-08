a0dc7479e4f4bdb7e442b31375d8ce8790296908
anjosuks <anjosuks@student.jyu.fi>
2025-10-08 19:49:55 +0300

    Lisätty paneeli vasemmalle

---
+++ FirstApp/src/paneeli.jsx
@@ -0,0 +1,15 @@
+export default function Paneeli() {
+  return (
+    <div
+      style={{
+        width: "300px",
+        backgroundColor: "rgba(19, 18, 18, 1)",
+        padding: "1rem",
+        borderRight: "1px solid #ccc",
+      }}
+    >
+      <h3>Parkkipaikat</h3>
+      <p>Tähän tulee valintoja, joilla voi rajata näytettäviä parkkipaikkoja</p>
+    </div>
+  );
+}
