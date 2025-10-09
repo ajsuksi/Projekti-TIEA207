4d9434a6500cda8610a8e79db9c932e2235b775e
aaahalom <anssi.a.halomo@student.jyu.fi>
2025-10-09 13:50:19 +0300

    korjattu

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express"
 import mongoose from "mongoose";
 import cors from "cors";
 
-const express = require('express');
+
 const app = express();
 app.use(cors());
 app.use(express.json()); //Sallii json datan vastaanoton
