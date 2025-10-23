df7e349cb2599ae7f18162a7afa0d8235be6bcb7
Kajander Taru Johanna <taru.j.kajander@student.jyu.fi>
2025-10-23 12:53:25 +0300

    Siistitty koodista turhat kommentit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,3 @@
-//import express from "express";
-//import cors from "cors";
-//import dotenv from "dotenv";
 
 require("dotenv").config();
 const express = require("express");
@@ -9,12 +6,12 @@ app.use(express.json()); // Sallii JSON-datan vastaanoton
 const cors = require("cors");
 app.use(cors());
 
-//yhdistä mongodb
+//yhdistä Mongodb
 const connectDB = require("./db.js");
 const placesModel = require("./models/places.js");
 connectDB(); //yhdistä database
 
-//Get ja Post ->>> MUUTA
+//Get Method
 app.get("/api/places", async (req, res) => {
   try {
     const places = await placesModel.find();
@@ -22,13 +19,7 @@ app.get("/api/places", async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Tietojen haku epäonnistui" });
   }
-  //res.send("<h1>Tervetuloa  API:in!");
 });
-
-/*  Hae kaikki paikat
-app.get("/api/places", (req, res) => {
-  res.json(places);
-}); */
 
 // Hae yksittäinen paikka ID:n perusteella
 app.get("/api/places/:id", async (req, res) => {
@@ -41,14 +32,8 @@ app.get("/api/places/:id", async (req, res) => {
   }
 });
 
-/* // Lisää uusi paikka
-const generateId = () => {
-  const maxId =
-    places.length > 0 ? Math.max(...places.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-}; */
 
-//TODO:
+//Post method
 app.post("/api/places", async (request, response) => {
   console.log("inside post function");
   const body = request.body;
@@ -57,7 +42,6 @@ app.post("/api/places", async (request, response) => {
     tyyppi: body.tyyppi,
     maksu: body.maksu,
     maksutapa: body.maksutapa,
-    //id: generateId(),
   });
 
   const val = await place.save();
