17939a4991b458fdc6c16afe96c236fbed6a0f01
anjosuks <anjosuks@student.jyu.fi>
2025-10-27 19:42:20 +0200

    Lisätty sivupaneeliin valintoja

--- FirstApp/src/paneeli.jsx
+++ FirstApp/src/paneeli.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 export default function Paneeli() {
+  const [onMaksullinen, asetaMaksullinen] = useState(false);
+  const [onIlmainen, asetaIlmainen] = useState(false);
+
   return (
     <div
       style={{
@@ -7,7 +12,86 @@ export default function Paneeli() {
       }}
     >
       <h3>Parkkipaikat</h3>
-      <p>Tähän tulee valintoja, joilla voi rajata näytettäviä parkkipaikkoja</p>
+      <div>
+        <strong>Tyyppi:</strong>
+        <br />
+        <label>
+          <input type="checkbox" name="kadunvarsi" />
+          Kadunvarsi
+        </label>
+        <br />
+
+        <label>
+          <input type="checkbox" name="pysakointihalli" />
+          Pysäköintihalli
+        </label>
+        <br />
+
+        <label>
+          <input type="checkbox" name="parkkipaikka" />
+          Parkkipaikka
+        </label>
+        <br />
+
+        <label>
+          <input type="checkbox" name="muu" />
+          Muu
+        </label>
+      </div>
+      <br />
+      
+      <div>
+        <strong>Maksullisuus:</strong>
+        <br />
+
+        <label>
+          <input
+            type="checkbox"
+            name="ilmainen"
+            checked={onIlmainen}
+            onChange={() => asetaIlmainen(!onIlmainen)}
+          />
+          Ilmainen
+        </label>
+        <br />
+
+        <label>
+          <input
+            type="checkbox"
+            name="maksullinen"
+            checked={onMaksullinen}
+            onChange={() => asetaMaksullinen(!onMaksullinen)}
+          />
+          Maksullinen
+        </label>
+        <br />
+      </div>
+      <br />
+
+{ onMaksullinen && (
+  <>
+      <div>
+        <strong>Maksutapa:</strong>
+        <br />
+        <label>
+          <input type="checkbox" name="kortti" />
+          Kortti
+        </label>
+        <br />
+
+        <label>
+          <input type="checkbox" name="kateinen" />
+          Käteinen
+        </label>
+        <br />
+
+        <label>
+          <input type="checkbox" name="moovy" />
+          Moovy
+        </label>
+        <br />
+      </div>
+      </>)}
     </div>
   );
 }
