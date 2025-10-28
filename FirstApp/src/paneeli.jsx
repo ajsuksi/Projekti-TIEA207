fdc24f4e38bd51d61fb935edbe6b2c7f7413d7d1
anjosuks <anjosuks@student.jyu.fi>
2025-10-28 11:36:21 +0200

    Sivupaneelia päivitetty

--- FirstApp/src/paneeli.jsx
+++ FirstApp/src/paneeli.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
 
 export default function Paneeli() {
-  const [onMaksullinen, asetaMaksullinen] = useState(false);
-  const [onIlmainen, asetaIlmainen] = useState(false);
+  const [onMaksullinen, asetaMaksullinen] = useState(true);
+  const [onIlmainen, asetaIlmainen] = useState(true);
 
   return (
     <div
       style={{
-        width: "250px",
+        position: "absolute",
+        zIndex: 1000,
+        width: "10vw",
+        left: 0,
+        top: 0,
+        height: "100vh",
         borderRight: "1px solid #ccc",
+        /* paddingLeft: "1rem", */
+        padding: "1rem",
+        transform: "translateX(-9vw)", // Piilotetaan (10vw - 9vw kahva)
+        transition: "transform 0.3s ease-in-out",
+        backgroundColor: "#222",
+        color: "white",
+      }}
+      onMouseEnter={(e) => {
+        e.currentTarget.style.transform = "translateX(0)"; // Näytetään paneeli
+      }}
+      onMouseLeave={(e) => {
+        e.currentTarget.style.transform = "translateX(-9vw)"; // Piilotetaan paneeli
       }}
     >
       <h3>Parkkipaikat</h3>
@@ -16,25 +33,25 @@ export default function Paneeli() {
         <strong>Tyyppi:</strong>
         <br />
         <label>
-          <input type="checkbox" name="kadunvarsi" />
+          <input type="checkbox" name="kadunvarsi" defaultChecked />
           Kadunvarsi
         </label>
         <br />
 
         <label>
-          <input type="checkbox" name="pysakointihalli" />
+          <input type="checkbox" name="pysakointihalli" defaultChecked />
           Pysäköintihalli
         </label>
         <br />
 
         <label>
-          <input type="checkbox" name="parkkipaikka" />
+          <input type="checkbox" name="parkkipaikka" defaultChecked />
           Parkkipaikka
         </label>
         <br />
 
         <label>
-          <input type="checkbox" name="muu" />
+          <input type="checkbox" name="muu" defaultChecked />
           Muu
         </label>
       </div>
@@ -74,19 +91,19 @@ export default function Paneeli() {
         <strong>Maksutapa:</strong>
         <br />
         <label>
-          <input type="checkbox" name="kortti" />
+          <input type="checkbox" name="kortti" defaultChecked />
           Kortti
         </label>
         <br />
 
         <label>
-          <input type="checkbox" name="kateinen" />
+          <input type="checkbox" name="kateinen" defaultChecked />
           Käteinen
         </label>
         <br />
 
         <label>
-          <input type="checkbox" name="moovy" />
+          <input type="checkbox" name="moovy" defaultChecked />
           Moovy
         </label>
         <br />
