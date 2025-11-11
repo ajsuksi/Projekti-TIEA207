4e953fd9c5e882e357affd8699881125a9317550
anjosuks <anjosuks@student.jyu.fi>
2025-11-11 17:22:01 +0200

    viewpopup päivitetty

--- FirstApp/src/ViewPopup.jsx
+++ FirstApp/src/ViewPopup.jsx
@@ -1,14 +1,25 @@
-export default function ViewPopup({ marker }) {
+export default function ViewPopup({ marker, onEdit }) {
   return (
     <div style={{ minWidth: "150px" }}>
-      <h4 style={{ marginTop: 0 }}>{marker.tyyppi}</h4>
-      <p>
-        {marker.maksu ? "Maksullinen" : "Ilmainen"}<br />
-        {marker.hinta && `Hinta: ${marker.hinta} €`}<br />
-        {marker.maksutapa?.length > 0 && `Maksutavat: ${marker.maksutapa.join(", ")}`}<br />
-        {marker.aikarajoitus && `Aikarajoitus: ${marker.aikarajoitus}`}<br />
-        {marker.lisatiedot && `Lisätiedot: ${marker.lisatiedot}`}
-      </p>
+      <h4 style={{ marginTop: 0, marginBottom: "8px", fontSize: "16px" }}>{marker.tyyppi}</h4>
+        {marker.maksu !== undefined && (<p>{marker.maksu ? "Maksullinen" : "Ilmainen"}</p>)}
+        {marker.hinta && (<p>Hinta: {marker.hinta}</p>)}
+        {marker.maksutapa?.length > 0 && (<p>Maksutavat: {marker.maksutapa.join(", ")}</p>)}
+        {marker.aikarajoitus && (<p>Aikarajoitus: {marker.aikarajoitus}</p>)}
+        {marker.lisatiedot && (<p>Lisätiedot: {marker.lisatiedot}</p>)}
+      <button
+        onClick={() => onEdit(marker)}
+        style={{
+          width: "100%",
+          padding: "8px",
+          backgroundColor: "#BDAEA3",
+          color: "black",
+          borderRadius: "4px",
+          cursor: "pointer",
+        }}
+        >
+        Muokkaa
+      </button>
     </div>
   );
 }
