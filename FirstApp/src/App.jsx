e165391e48cb09e078c94e590437f22ce9dcae44
aaahalom <anssi.a.halomo@student.jyu.fi>
2025-11-27 12:45:32 +0200

    logo lisätty paneeliin ja sivun avaukseen

--- FirstApp/src/App.jsx
+++ FirstApp/src/App.jsx
@@ -1,6 +1,13 @@
 import ParkingMap from "./map";
+import { useState } from "react";
+import SplashScreen from "./SplashScreen";
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  if (showSplash) {
+    return <SplashScreen onFinish={() => setShowSplash(false)} />;
+  }
   return (
     <div>
       <ParkingMap />
---
+++ FirstApp/src/SplashScreen.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+export default function SplashScreen({ onFinish }) {
+  useEffect(() => {
+    const timer = setTimeout(() => onFinish(), 1500);
+    return () => clearTimeout(timer);
+  }, [onFinish]);
+
+  return (
+    <div style={{
+      position: "fixed",
+      inset: 0,
+      background: "#ffffffff",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      zIndex: 9999
+    }}>
+      <img
+        src="/src/icons/logo.png"
+        alt="Parkkisovellus"
+        style={{ width: "180px", opacity: 1 }}
+      />
+    </div>
+  );
+}
